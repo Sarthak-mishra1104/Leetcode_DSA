be8d73f5ffcce64a0f64e3cc36510436e5af08c9
Sarthak Mishra <Sarthakmishra.2431078@gmail.com>
2026-07-13 22:07:17 +0530

    Time: 0 ms (100%), Space: 31.2 MB (95.62%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void inorder(TreeNode* root, int &count){
+        if(root == nullptr)
+            return;
+
+        inorder(root->left, count);
+        count++;
+        inorder(root->right, count);
+    }
+
+    int countNodes(TreeNode* root) {
+        int count = 0;
+        inorder(root, count);
+        return count;
+    }
+};
